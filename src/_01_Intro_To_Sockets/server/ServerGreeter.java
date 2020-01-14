e9a008a91e0694aaf59bef349f7287aa5ffbbf9a
League Student <league@iMac-27.attlocal.net>
2020-01-13 16:57:13 -0800

    working on chat app

--- src/_01_Intro_To_Sockets/server/ServerGreeter.java
+++ src/_01_Intro_To_Sockets/server/ServerGreeter.java
@@ -5,50 +5,63 @@ import java.io.*;
 
 public class ServerGreeter extends Thread {
 	//1. Create an object of the ServerSocket class
-
+	ServerSocket serverSocket;
 	public ServerGreeter() throws IOException {
 		//2. Initialize the ServerSocket object. In the parameters,
 		//   you must define the port at which the server will listen for connections.
-		
+		serverSocket = new ServerSocket();
 		//*OPTIONAL* you can set a time limit for the server to wait by using the 
 		//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
+		serverSocket.setSoTimeout(10000);
 	}
 
 	public void run() {
 		//3. Create a boolean variable and initialize it to true.
-		
+		boolean bool = true;
 		//4. Make a while loop that continues looping as long as the boolean created in the previous step is true.
-			
+			while(bool) {
 			//5. Make a try-catch block that checks for two types Exceptions: SocketTimeoutException and IOException.
 			//   Put steps 8 - 15 in the try block.
-		
+			try {
 				//8. Let the user know that the server is waiting for a client to connect.
-		
+				System.out.println("server waiting for a client to connect");
 				//9. Create an object of the Socket class and initialize it to serverSocket.accept();
 				//   Change serverSocket to match the ServerSocket member variable you created in step 1.
 				//   The program will wait her until either a client connects or the timeout expires.
-
+				Socket sock = serverSocket.accept();
 				//10. Let the user know that the client has connected.
-				
+				System.out.println("client connected");
 				//11. Create a DataInputStream object. When initializing it, use the Socket object you created in step 9 to call the getInputStream() method.
-
+				DataInputStream dis = (DataInputStream) sock.getInputStream();
 				//12. Print the message from the DataInputStream object using the readUTF() method
-				
+				dis.readUTF();
 				//13. Create a DataOutputStream object. When initializing it, use the Server object you created in step 9 to call the getOutputStream() method.
-				
+				DataOutputStream dos = (DataOutputStream) sock.getOutputStream();
 				//14. Use the DataOutputStream object to send a message to the client using the writeUTF(String message) method.
-				
+				dos.writeUTF("this message is being sent");
 				//15. Close the client server
-		
+			} catch (SocketTimeoutException e) {
 			//6. If the program catches a SockeTimeoutException, let the user know about it and set loop's boolean variable to false.
-
+					bool = false;
+					System.out.println("SockeTimeoutException: " + e);
+			} catch (IOException e) {
 			//7. If the program catches a IOException, let the user know about it and set the loop's boolean variable to false.
-
-		
+					bool = false;
+					System.out.println("IOException: " + e);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		//16. In a new thread, create an object of the ServerGreeter class and start the thread. Don't forget the try-catch.
-		
+		Thread tred = new Thread(()-> {
+			try {
+				ServerGreeter sg = new ServerGreeter();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		tred.start();
 	}
 }
--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,19 @@
 package _02_Chat_Application;
 
+import _00_Click_Chat.gui.ButtonClicker;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
+
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp() {
+		System.out.println("hi");
+	}
 
 }
