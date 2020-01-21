b1b16f58ed7d7e085bcbe9eb35090f1c6411cc5d
League Student <league@iMac-15.attlocal.net>
2020-01-20 16:46:21 -0800

    chat application (IP)

--- src/_01_Intro_To_Sockets/server/ServerGreeter.java
+++ src/_01_Intro_To_Sockets/server/ServerGreeter.java
@@ -6,13 +6,15 @@ import java.io.*;
 public class ServerGreeter extends Thread {
 	//1. Create an object of the ServerSocket class
 	ServerSocket serverSocket;
+	Socket sock = null;
+	
 	public ServerGreeter() throws IOException {
 		//2. Initialize the ServerSocket object. In the parameters,
 		//   you must define the port at which the server will listen for connections.
-		serverSocket = new ServerSocket();
+		serverSocket = new ServerSocket(8080,100);
 		//*OPTIONAL* you can set a time limit for the server to wait by using the 
 		//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
-		serverSocket.setSoTimeout(10000);
+		serverSocket.setSoTimeout(30000);
 	}
 
 	public void run() {
@@ -25,20 +27,27 @@ public class ServerGreeter extends Thread {
 			try {
 				//8. Let the user know that the server is waiting for a client to connect.
 				System.out.println("server waiting for a client to connect");
+				
 				//9. Create an object of the Socket class and initialize it to serverSocket.accept();
 				//   Change serverSocket to match the ServerSocket member variable you created in step 1.
 				//   The program will wait her until either a client connects or the timeout expires.
-				Socket sock = serverSocket.accept();
+				sock = serverSocket.accept();
+				
 				//10. Let the user know that the client has connected.
 				System.out.println("client connected");
+				
 				//11. Create a DataInputStream object. When initializing it, use the Socket object you created in step 9 to call the getInputStream() method.
-				DataInputStream dis = (DataInputStream) sock.getInputStream();
+				DataInputStream dis = new DataInputStream(sock.getInputStream());
+				
 				//12. Print the message from the DataInputStream object using the readUTF() method
-				dis.readUTF();
+				System.out.println(dis.readUTF());
+				
 				//13. Create a DataOutputStream object. When initializing it, use the Server object you created in step 9 to call the getOutputStream() method.
-				DataOutputStream dos = (DataOutputStream) sock.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+				
 				//14. Use the DataOutputStream object to send a message to the client using the writeUTF(String message) method.
 				dos.writeUTF("this message is being sent");
+				
 				//15. Close the client server
 			} catch (SocketTimeoutException e) {
 			//6. If the program catches a SockeTimeoutException, let the user know about it and set loop's boolean variable to false.
@@ -54,14 +63,10 @@ public class ServerGreeter extends Thread {
 
 	public static void main(String[] args) {
 		//16. In a new thread, create an object of the ServerGreeter class and start the thread. Don't forget the try-catch.
-		Thread tred = new Thread(()-> {
 			try {
-				ServerGreeter sg = new ServerGreeter();
+				new Thread(new ServerGreeter() ).start();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		});
-		tred.start();
 	}
 }
--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,19 +1,66 @@
 package _02_Chat_Application;
 
+import java.awt.LayoutManager;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import _00_Click_Chat.gui.ButtonClicker;
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
 
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
-
+public class ChatApp extends JFrame {
+	JPanel panel;
+	
+	JTextField tField = new JTextField(15);
+	JLabel label = new JLabel("message");
+	JButton b = new JButton("send");
+	
+	Server server;
+	Client client;
+	
 	public static void main(String[] args) {
 		new ChatApp();
 	}
 	
 	public ChatApp() {
-		System.out.println("hi");
+		boolean host = JOptionPane.showInputDialog(null, "Host a connection? ('Yes'/'No')").equals("Yes") ? true : false;
+		if(host) {
+			server = new Server(8080);
+			setTitle("SERVER");
+			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			panel = new JPanel();
+			add(panel);
+			panel.add(label);
+			panel.add(tField);
+			panel.add(b);
+			setVisible(true);
+			setSize(400, 300);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			pack();
+			server.start();
+		} else {
+			setTitle("CLIENT");
+			String ipAd = JOptionPane.showInputDialog("what is IP adress");
+			String pNum = JOptionPane.showInputDialog("what is port number");
+			add(panel);
+			panel.add(label);
+			panel.add(tField);
+			panel.add(b);
+			setVisible(true);
+			setSize(400, 300);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			client.start();
+		}
 	}
 
 }
